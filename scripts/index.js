74b5b27b8f3e26436e9d93f388c55e59d705ddbf
nomsa25116 <ntnongcula@gmail.com>
2025-09-28 23:34:20 +0200

    updated HTML

---
+++ scripts/index.js
@@ -0,0 +1,18 @@
+import { podcasts } from "./data.js";
+import { createModal } from "./components/modal.js";
+import { createGrid } from "./components/createGrid.js";
+
+/**
+ * Initializes the podcast application.
+ *
+ * @principle SRP - Only responsible for application startup logic like event binding and rendering initial grid.
+ */
+function init() {
+  document
+    .getElementById("closeModal")
+    .addEventListener("click", createModal.close);
+  const grid = createGrid();
+  grid.render(podcasts);
+}
+
+init();
